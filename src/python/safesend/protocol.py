36789c9d4444b6addc01d4fb38dcf066b2ced27d
Subomiadekunle <subomiade25@gmail.com>
2025-10-22 15:52:21 -0500

    protocol.py

--- src/python/safesend/protocol.py
+++ src/python/safesend/protocol.py
@@ -1,8 +1,9 @@
+# src/python/safesend/protocol.py
 from dataclasses import dataclass
 
 PROTOCOL_VERSION = 1
 DEFAULT_PORT = 9000
-CHUNK_SIZE = 64 * 1024  # 64KB
+CHUNK_SIZE = 64 * 1024  # 64KB (tune later)
 
 @dataclass
 class ChunkHeader:
